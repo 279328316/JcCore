932e52b7bf7e36b6f865a6c9113a81e06577418f
zhangjiahua <279328316@qq.com>
2019-10-31 11:30:55 +0800

    ApiTest

--- Jc.Core.TestApp/Test/ApiTest.cs
+++ Jc.Core.TestApp/Test/ApiTest.cs
@@ -10,9 +10,11 @@ namespace Jc.Core.TestApp.Test
         {
             string keywords = "Test";
             List<string> areas = new List<string>();
-            List<int> levels = new List<int>() { 1,2};
+            List<int> levels = new List<int>() { 1, 2 };
+            List<Guid?> ids = new List<Guid?>() { Guid.Parse("1140f378-d1a3-407e-8864-68c2dab2ae52") };
             List<ApiDto> list = Dbc.Db.GetList<ApiDto>(a => areas.Contains(a.Area));
             list = Dbc.Db.GetList<ApiDto>(a => levels.Contains(a.ApiLevel.Value));
+            list = Dbc.Db.GetList<ApiDto>(a => ids.Contains(a.Id));
             list = Dbc.Db.GetList<ApiDto>(a => a.Name.Contains(keywords.ToString()));
         }
     }
