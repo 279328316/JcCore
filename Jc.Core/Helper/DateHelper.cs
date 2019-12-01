0e997e08551b25770f224220e7272ddaefcd58f4
jiahua zhang <279328316@qq.com>
2019-12-01 18:05:02 +0800

    DateHelper修改

--- Jc.Core/Helper/DateHelper.cs
+++ Jc.Core/Helper/DateHelper.cs
@@ -6,11 +6,13 @@ using System.IO;
 
 namespace Jc.Core.Helper
 {
+    /// <summary>
+    /// Date Helper
+    /// </summary>
     public class DateHelper
     {
         /// <summary>
-        /// 计算日期时间差
-        /// hh:mm:ss
+        /// 计算日期时间差 hh:mm:ss
         /// </summary>
         /// <param name="dt1"></param>
         /// <param name="dt2"></param>
@@ -25,18 +27,38 @@ namespace Jc.Core.Helper
             {
                 dt2 = DateTime.Now;
             }
-            TimeSpan ts = dt2.Value - dt1.Value;
+            return DateDif(dt2.Value - dt1.Value);
+        }
+
+        /// <summary>
+        /// 计算日期时间 hh:mm:ss
+        /// </summary>
+        /// <param name="ticks">ticks</param>
+        /// <returns></returns>
+        public static string DateDif(long ticks)
+        {
+            TimeSpan ts = new TimeSpan(ticks);
+            return DateDif(ts);
+        }
+
+        /// <summary>
+        /// 计算日期时间 hh:mm:ss
+        /// </summary>
+        /// <param name="ts">TimeSpan</param>
+        /// <returns></returns>
+        public static string DateDif(TimeSpan ts)
+        {
             int hours = (int)ts.TotalHours;//总时间分差
-            int minutes = (int)(ts.TotalMinutes- hours*60);//总时间分差
-            int seconds = (int)(ts.TotalSeconds - hours* 3600 - minutes * 60);//总时间分差
+            int minutes = (int)(ts.TotalMinutes - hours * 60);//总时间分差
+            int seconds = (int)(ts.TotalSeconds - hours * 3600 - minutes * 60);//总时间分差
 
             string result = string.Format("{0}:{1}:{2}",
-                hours.ToString().PadLeft(2,'0'),
+                hours.ToString().PadLeft(2, '0'),
                 minutes.ToString().PadLeft(2, '0'),
                 seconds.ToString().PadLeft(2, '0'));
             return result;
         }
-
+        
         /// <summary>
         /// 计算日期时间差
         /// d天 hh:mm:ss
@@ -54,7 +76,27 @@ namespace Jc.Core.Helper
             {
                 dt2 = DateTime.Now;
             }
-            TimeSpan ts = dt2.Value - dt1.Value;
+            return DateDifDay( dt2.Value - dt1.Value);
+        }
+
+        /// <summary>
+        /// 计算日期时间 d天 hh:mm:ss
+        /// </summary>
+        /// <param name="ticks">ticks</param>
+        /// <returns></returns>
+        public static string DateDifDay(long ticks)
+        {
+            TimeSpan ts = new TimeSpan(ticks);
+            return DateDifDay(ts);
+        }
+
+        /// <summary>
+        /// 计算日期时间差 d天 hh:mm:ss
+        /// </summary>
+        /// <param name="ts">TimeSpan</param>
+        /// <returns></returns>
+        public static string DateDifDay(TimeSpan ts)
+        {
             int days = (int)ts.TotalDays;
             int hours = (int)(ts.TotalHours - days * 24);//总时间分差
             int minutes = (int)(ts.TotalMinutes - days * 24 * 60 - hours * 60);//总时间分差
