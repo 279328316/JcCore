914f01b638aa69b552e40133d01bb4f1ab5b330d
jiahua zhang <279328316@qq.com>
2019-12-19 11:32:01 +0800

    ConfigHelper添加GetSectionSetting

--- Jc.Core/Helper/ConfigHelper.cs
+++ Jc.Core/Helper/ConfigHelper.cs
@@ -94,5 +94,46 @@ namespace Jc.Core.Helper
             }
             return value;
         }
+
+        /// <summary>
+        /// 获取配置
+        /// </summary>
+        /// <param name="section">section</param>
+        /// <param name="key">key</param>
+        /// <param name="configFileName">配置文件名称</param>
+        /// <returns></returns>
+        public static string GetSectionSetting(string section,string key, string configFileName = null)
+        {
+            string value = null;
+            string filePath;
+            if (string.IsNullOrEmpty(configFileName))
+            {
+                configFileName = "appsettings.json";
+                filePath = Path.Combine(AppContext.BaseDirectory, configFileName);
+            }
+            else
+            {
+                if (File.Exists(configFileName))
+                {
+                    filePath = configFileName;
+                }
+                else
+                {
+                    filePath = Path.Combine(AppContext.BaseDirectory, configFileName);
+                }
+            }
+            if (File.Exists(filePath))
+            {
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile(filePath);
+                IConfiguration configuration = builder.Build();
+                IConfigurationSection configSection = configuration.GetSection(section);
+                value = configSection[key];
+            }
+            else
+            {   //兼容 .netFramework应用程序
+                value = ((IConfigurationSection)ConfigurationManager.GetSection(section))[key];
+            }
+            return value;
+        }
     }
 }
