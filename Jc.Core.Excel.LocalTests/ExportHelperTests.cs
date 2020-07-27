48db54e9c8c42004919e785d3c50b06dcb5fac8d
jiahua zhang <279328316@qq.com>
2020-07-27 14:41:52 +0800

    Jc.Core.Excel Project Add

---
+++ Jc.Core.Excel.LocalTests/ExportHelperTests.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jc.Core.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Jc.Core.Excel.Tests
+{
+    [TestClass()]
+    public class ExportHelperTests
+    {
+        [TestMethod()]
+        public void ExportToFileTest()
+        {
+            List<VwAppointmentDto> appointments = GetAppointmentList();
+
+            List<FieldMap> fieldMaps = GetExportFiledMapList();
+
+            string filePath = @"d:\1.xlsx";
+
+            ExcelHelper.ExportToFile(appointments,fieldMaps, filePath);
+        }
+
+
+        [TestMethod()]
+        public void ExportToMemoryStreamTest()
+        {
+            List<VwAppointmentDto> appointments = GetAppointmentList();
+
+            List<FieldMap> fieldMaps = GetExportFiledMapList();
+
+            byte[] buffer;
+            using (MemoryStream ms = ExcelHelper.ExportToMemoryStream(appointments, fieldMaps))
+            {
+                buffer = ms.ToArray();
+
+                string filePath = @"d:\1.xlsx";
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(buffer, 0, buffer.Length);
+                    fs.Flush();
+                }
+                ms.Close();
+            }
+        }
+
+
+
+        /// <summary>
+        /// 获取预约数据
+        /// </summary>
+        /// <returns></returns>
+        private List<VwAppointmentDto> GetAppointmentList()
+        {
+            List<VwAppointmentDto> list = new List<VwAppointmentDto>()
+            {
+                new VwAppointmentDto()
+                {
+                    PatientName = "张三",
+                    PatientSex = Sex.Male,
+                    Birthday = new DateTime(1965,1,31),
+                    Height = 172,
+                    Weight = 65,
+                    IsDiabetic = null,
+                    IdCardType = IdCardType.IdCard,
+                    IdCardNumber = "123456789",
+                    PhoneNumber = "185012412251",
+                    AppointmentTime = DateTime.Now.Date.AddDays(3),
+                    AppointmentState = AppointmentState.Confirmed
+                },
+                new VwAppointmentDto()
+                {
+                    PatientName = "李四",
+                    PatientSex = Sex.Male,
+                    Birthday = new DateTime(1975,1,31),
+                    Height = 177,
+                    Weight = 67,
+                    IsDiabetic = true,
+                    IdCardType = IdCardType.IdCard,
+                    IdCardNumber = "123456789",
+                    PhoneNumber = "187012412251",
+                    AppointmentTime = DateTime.Now.Date.AddDays(5),
+                    AppointmentState = AppointmentState.Confirmed
+                },
+                new VwAppointmentDto()
+                {
+                    PatientName = "王五",
+                    PatientSex = Sex.FeMale,
+                    Birthday = new DateTime(1985,1,31),
+                    Height = 182,
+                    Weight = 67,
+                    IsDiabetic = false,
+                    IdCardType = IdCardType.IdCard,
+                    IdCardNumber = "123456789",
+                    PhoneNumber = "189012412251",
+                    AppointmentTime = DateTime.Now.Date.AddDays(6),
+                    AppointmentState = AppointmentState.Confirmed
+                },
+            };
+            return list;
+        }
+
+        /// <summary>
+        /// 获取导出字段Map
+        /// </summary>
+        private List<FieldMap> GetExportFiledMapList()
+        {
+            FieldMapBuilder builder = new FieldMapBuilder();
+
+            builder.Add(
+                new FieldRowIndexMap(),
+                new FieldMap<VwAppointmentDto>(a => a.AppointmentTime, "预约日期", FieldType.DateTime, "yyyy-MM-dd"),
+                new FieldMap<VwAppointmentDto>(a => a.AppointmentTime, "预约时间", FieldType.DateTime, "HH:mm:ss"),
+                new FieldMap<VwAppointmentDto>(a => a.ExamNumber, "检查号"),
+                new FieldMap<VwAppointmentDto>(a => a.PatientName, "患者姓名"),
+                new FieldMap<VwAppointmentDto>(a => a.ExamItemName, "检查项目"),
+                new FieldMap<VwAppointmentDto>(a => a.AppointmentStateName, "预约状态"),
+                new FieldMap<VwAppointmentDto>(a => a.Age, "患者年龄"),
+                new FieldMap<VwAppointmentDto>(a => a.Birthday, "出生日期", FieldType.DateTime, "yyyy-MM-dd"),
+                new FieldMap<VwAppointmentDto>(a => a.SexName, "患者性别"),
+                new FieldMap<VwAppointmentDto>(a => a.Height, "患者身高"),
+                new FieldMap<VwAppointmentDto>(a => a.Weight, "患者体重"),
+                new FieldMap<VwAppointmentDto>(a => a.IsDiabetic, "是否糖尿病患者",FieldType.Boolean,"是|"),
+                new FieldMap<VwAppointmentDto>(a => a.Diagnosis, "诊断"),
+                new FieldMap<VwAppointmentDto>(a => a.IdCardTypeName, "证件类型"),
+                new FieldMap<VwAppointmentDto>(a => a.IdCardNumber, "证件号"),
+                new FieldMap<VwAppointmentDto>(a => a.PhoneNumber, "手机号"),
+                new FieldMap<VwAppointmentDto>(a => a.Note, "备注"));
+            List<FieldMap> fieldMaps = builder.GetFieldMaps();
+
+            return fieldMaps;
+        }
+    }
+}
---
+++ Jc.Core.Excel.LocalTests/Model/VwAppointmentDto.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Jc.Core;
+
+namespace Jc.Core.Excel.Tests
+{
+    /// <summary>
+    /// VIEW Dto
+    /// </summary>
+    [Table(Name = "vw_appointment",DisplayText = "VIEW")]
+    public class VwAppointmentDto
+    {
+        #region Properties
+        /// <summary>
+        /// Id
+        /// </summary>
+        [PkField]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 预约用户Id
+        /// </summary>
+        public Guid? UserId { get; set; }
+
+        /// <summary>
+        /// 创建方式 1微信 2管理后台
+        /// </summary>
+        public AppointmentCreateType CreateType { get; set; }
+
+        /// <summary>
+        /// 创建方式名称
+        /// </summary>
+        [NoMapping]
+        public string CreateTypeName { get { return CreateType.GetDisplayName(); } }
+
+        /// <summary>
+        /// 就诊人Id
+        /// </summary>
+        public Guid? AppointmentPersonId { get; set; }
+
+        /// <summary>
+        /// 患者姓名
+        /// </summary>
+        public string PatientName { get; set; }
+
+        /// <summary>
+        /// 患者性别
+        /// </summary>
+        public Sex PatientSex { get; set; }
+
+        /// <summary>
+        /// 性别名称
+        /// </summary>
+        [NoMapping]
+        public string SexName { get { return PatientSex.GetDisplayName(); } }
+
+        /// <summary>
+        /// 患者出生日期
+        /// </summary>
+        public DateTime? Birthday { get; set; }
+
+        /// <summary>
+        /// 患者年龄
+        /// </summary>
+        [NoMapping]
+        public int? Age 
+        { 
+            get 
+            {
+                int? age = null;
+                if (Birthday.HasValue)
+                {
+                    age = DateTime.Now.Year - Birthday.Value.Year;
+                }
+                return age;
+            } 
+        }
+
+        /// <summary>
+        /// 身高(cm)
+        /// </summary>
+        public decimal? Height { get; set; }
+
+        /// <summary>
+        /// 体重(kg)
+        /// </summary>
+        public decimal? Weight { get; set; }
+
+        /// <summary>
+        /// 是否为糖尿病患者
+        /// </summary>
+        public bool? IsDiabetic { get; set; }
+
+        /// <summary>
+        /// 诊断
+        /// </summary>
+        public string Diagnosis { get; set; }
+
+        /// <summary>
+        /// 证件类型
+        /// </summary>
+        public IdCardType IdCardType { get; set; }
+
+        /// <summary>
+        /// 证件类型名称
+        /// </summary>
+        [NoMapping]
+        public string IdCardTypeName { get { return IdCardType.GetDisplayName(); } }
+
+        /// <summary>
+        /// 证件号
+        /// </summary>
+        public string IdCardNumber { get; set; }
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// 常住地址
+        /// </summary>
+        public string CommonAddress { get; set; }
+
+        /// <summary>
+        /// 当前住址
+        /// </summary>
+        public string CurrentAddress { get; set; }
+
+        /// <summary>
+        /// 联系人姓名
+        /// </summary>
+        public string ContactName { get; set; }
+
+        /// <summary>
+        /// 联系人手机号
+        /// </summary>
+        public string ContactPhoneNumber { get; set; }
+
+        /// <summary>
+        /// 联系人证件类型
+        /// </summary>
+        public IdCardType ContactIdCardType { get; set; }
+
+        /// <summary>
+        /// 联系人证件类型名称
+        /// </summary>
+        [NoMapping]
+        public string ContactIdCardTypeName { get { return ContactIdCardType.GetDisplayName(); } }
+
+        /// <summary>
+        /// 联系人证件号
+        /// </summary>
+        public string ContactIdCardNumber { get; set; }
+
+        /// <summary>
+        /// 检查号
+        /// </summary>
+        public string ExamNumber { get; set; }
+
+
+        /// <summary>
+        /// 检查项名称
+        /// </summary>
+        public string ExamItemName { get; set; }
+
+        /// <summary>
+        /// 邀请Code
+        /// </summary>
+        public string InvitationCode { get; set; }
+
+        /// <summary>
+        /// 预约时间
+        /// </summary>
+        public DateTime? AppointmentTime { get; set; }
+
+        /// <summary>
+        /// 预约状态
+        /// </summary>
+        public AppointmentState AppointmentState { get; set; }
+
+        /// <summary>
+        /// 预约状态名称
+        /// </summary>
+        [NoMapping]
+        public string AppointmentStateName { get { return AppointmentState.GetDisplayName(); } }
+
+        /// <summary>
+        /// 检查费用
+        /// </summary>
+        public decimal? ExamCost { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Note { get; set; }
+
+        /// <summary>
+        /// 任务添加人Id
+        /// </summary>
+        public Guid? AddUserId { get; set; }
+
+        /// <summary>
+        /// 任务添加时间
+        /// </summary>
+        public DateTime AddDate { get; set; }
+
+        /// <summary>
+        /// 最后更新人Id
+        /// </summary>
+        public Guid? LastUpdateUserId { get; set; }
+
+        /// <summary>
+        /// 最后更新时间
+        /// </summary>
+        public DateTime LastUpdateDate { get; set; }
+        #endregion
+    }
+}
