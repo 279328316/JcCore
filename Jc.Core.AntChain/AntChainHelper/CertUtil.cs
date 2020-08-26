496267c5efd5325c2774a9ad38de1a904e23122a
jiahua zhang <279328316@qq.com>
2020-08-26 16:05:38 +0800

    蚂蚁链调用调整

---
+++ Jc.Core.AntChain/AntChainHelper/CertUtil.cs
@@ -0,0 +1,55 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Jc.Core.AntChain
+{
+    public class CertUtil
+    {
+        public static string Sign(string contentForSign, string privateKey)
+        {
+            //转换秘钥
+            string netKey = GetRSAPrivateKey(privateKey);
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(netKey);
+            //创建一个空对象
+            RSACryptoServiceProvider rsaClear = new RSACryptoServiceProvider();
+            RSAParameters paras = rsa.ExportParameters(true);
+            rsaClear.ImportParameters(paras);
+            //签名返回
+            using (SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider())
+            {
+                byte[] signData = rsa.SignData(Encoding.UTF8.GetBytes(contentForSign), sha256);
+                return BytesToHex(signData);
+            }
+        }
+
+        public static string BytesToHex(byte[] data)
+        {
+            StringBuilder sbRet = new StringBuilder(data.Length * 2);
+            for (int i = 0; i < data.Length; i++)
+            {
+                sbRet.Append(Convert.ToString(data[i], 16).PadLeft(2, '0'));
+            }
+            return sbRet.ToString();
+        }
+
+        public static string GetRSAPrivateKey(string privateKey)
+        {
+            RsaPrivateCrtKeyParameters privateKeyParam = (RsaPrivateCrtKeyParameters)PrivateKeyFactory.CreateKey(Convert.FromBase64String(privateKey));
+
+            return string.Format("<RSAKeyValue><Modulus>{0}</Modulus><Exponent>{1}</Exponent><P>{2}</P><Q>{3}</Q><DP>{4}</DP><DQ>{5}</DQ><InverseQ>{6}</InverseQ><D>{7}</D></RSAKeyValue>",
+                Convert.ToBase64String(privateKeyParam.Modulus.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.PublicExponent.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.P.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.Q.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.DP.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.DQ.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.QInv.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.Exponent.ToByteArrayUnsigned()));
+        }
+    }
+}
---
+++ Jc.Core.AntChain/Program.cs
@@ -0,0 +1,91 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jc.Core.AntChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecordModel insertModel = new RecordModel() {
+                RecordId = "Sino-Test008",
+                ExamId = "00000000-0000-0000-0000-0000000006",
+                PatientName = "风清扬",
+                ExamNumber = "Wb35006",
+                ExamDate = DateTime.Parse("2020-08-13 16:43"),
+                UploadDate = DateTime.Parse("2020-08-13 18:43"),
+                Uploader = "赛诺联合检查中心",
+                Operation = "文件上传",
+                Operator = "小赛云",
+                PowerUser = "赛诺联合检查中心",
+                PowerDate = DateTime.Parse("2020-08-13 18:43"),
+                PowerNote = "检查数据上传",
+            };
+            string insertHash = SinoChainHelper.Instance.SetExamRecord(insertModel);
+            Console.WriteLine($"InsertHash:{insertHash}");
+
+            string insertHash1 = SinoChainHelper.Instance.SetExamRecord(insertModel);
+            Console.WriteLine($"InsertHash1:{insertHash1}");
+
+            RecordModel updateModel1 = new RecordModel()
+            {
+                RecordId = "Sino-Test008",
+                ExamId = "00000000-0000-0000-0000-0000000006",
+                PatientName = "风清扬",
+                ExamNumber = "Wb35006",
+                ExamDate = DateTime.Parse("2020-08-13 16:43"),
+                UploadDate = DateTime.Parse("2020-08-13 18:43"),
+                Uploader = "赛诺联合检查中心",
+                Operation = "数据下载",
+                Operator = "王医生",
+                PowerUser = "赛诺联合检查中心",
+                PowerDate = DateTime.Parse("2020-08-14 18:43"),
+                PowerNote = "读片医生读片授权",
+            };
+            string updateHash1 = SinoChainHelper.Instance.SetExamRecord(updateModel1);
+            Console.WriteLine($"updateHash1:{updateHash1}");
+
+            RecordModel updateModel2 = new RecordModel()
+            {
+                RecordId = "Sino-Test008",
+                ExamId = "00000000-0000-0000-0000-0000000006",
+                PatientName = "风清扬",
+                ExamNumber = "Wb35006",
+                ExamDate = DateTime.Parse("2020-08-13 16:43"),
+                UploadDate = DateTime.Parse("2020-08-13 18:43"),
+                Uploader = "赛诺联合检查中心",
+                Operation = "数据下载",
+                Operator = "风清扬",
+                PowerUser = "赛诺联合检查中心",
+                PowerDate = DateTime.Parse("2020-08-15 12:43"),
+                PowerNote = "用户下载影像数据",
+            };
+            string updateHash2 = SinoChainHelper.Instance.SetExamRecord(updateModel2);
+            Console.WriteLine($"updateHash2:{updateHash2}");
+
+
+            RecordModel updateModel3 = new RecordModel()
+            {
+                RecordId = "Sino-Test008",
+                ExamId = "00000000-0000-0000-0000-0000000006",
+                PatientName = "风清扬",
+                ExamNumber = "Wb35006",
+                ExamDate = DateTime.Parse("2020-08-13 16:43"),
+                UploadDate = DateTime.Parse("2020-08-13 18:43"),
+                Uploader = "赛诺联合检查中心",
+                Operation = "数据下载",
+                Operator = "张医生",
+                PowerUser = "赛诺联合检查中心",
+                PowerDate = DateTime.Parse("2020-08-15 18:43"),
+                PowerNote = "报告检查",
+            };
+            string updateHash3 = SinoChainHelper.Instance.SetExamRecord(updateModel3);
+            Console.WriteLine($"updateHash3:{updateHash3}");
+
+            Console.WriteLine("Press Any Key To Exit.");
+            Console.ReadKey();
+        }
+    }
+}
