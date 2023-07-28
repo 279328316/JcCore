b3f9720b0b01b0230491437d1bcdf52f45782e58
jiahua.zhang <279328316@qq.com>
2023-07-28 10:09:05 +0800

    Add xcopyFile.bat

---
+++ xcopyFile.bat
@@ -0,0 +1,43 @@
+echo off
+cls
+:: 设置变量不能有空格 等号左边不允许有空格，等号右边的所有包括空格会全部赋值给变量。
+set version=1.1.7
+echo User Dir %USERPROFILE%   FileVersion : %version%
+
+set curDir="%USERPROFILE%\.nuget\packages\jc.base\%version%\lib\netstandard2.0\"
+echo Target Dir : %curDir%
+Del "%curDir%\*" /q
+xcopy /Y "Jc.Base\bin\Debug\netstandard2.0" %curDir%
+
+set curDir="%USERPROFILE%\.nuget\packages\jc.core\%version%\lib\netstandard2.0\"
+echo Target Dir : %curDir%
+Del "%curDir%\*" /q
+xcopy /Y "Jc.Core\bin\Debug\netstandard2.0" %curDir%
+
+set curDir="%USERPROFILE%\.nuget\packages\jc.core.database\%version%\lib\netstandard2.0\"
+echo Target Dir : %curDir%
+Del "%curDir%\*" /q
+xcopy /Y "Jc.Core.Database\bin\Debug\netstandard2.0" %curDir%
+
+set curDir="%USERPROFILE%\.nuget\packages\jc.core.mssql\%version%\lib\netstandard2.0\"
+echo Target Dir : %curDir%
+Del "%curDir%\*" /q
+xcopy /Y "Jc.Core.MsSql\bin\Debug\netstandard2.0" %curDir%
+
+set curDir="%USERPROFILE%\.nuget\packages\jc.core.mysql\%version%\lib\netstandard2.0\"
+echo Target Dir : %curDir%
+Del "%curDir%\*" /q
+xcopy /Y "Jc.Core.MySql\bin\Debug\netstandard2.0" %curDir%
+
+set curDir="%USERPROFILE%\.nuget\packages\jc.core.postgresql\%version%\lib\netstandard2.0\"
+echo Target Dir : %curDir%
+Del "%curDir%\*" /q
+xcopy /Y "Jc.Core.PostgreSql\bin\Debug\netstandard2.0" %curDir%
+
+set curDir="%USERPROFILE%\.nuget\packages\jc.core.sqlite\%version%\lib\netstandard2.0\"
+echo Target Dir : %curDir%
+Del "%curDir%\*" /q
+xcopy /Y "Jc.Core.Sqlite\bin\Debug\netstandard2.0" %curDir%
+
+echo Copy Finished.
+pause 
