ed92718f084c8c0e7ce13950e1f5a36189d61d65
jiahua.zhang <279328316@qq.com>
2024-05-27 10:35:28 +0800

    添加Jc.Util.Excel Test

---
+++ Jc.Util.Excel.LocalTests/ExcelHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jc.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Jc.Util.Excel.LocalTests;
+
+namespace Jc.Excel.Tests
+{
+    [TestClass()]
+    public class ExcelHelperTests
+    {
+        [TestMethod()]
+        public void ExportToFileTest()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){ Name = "张三" , ClassName = "一年级一班" },
+                new Student(){ Name = "李四" , ClassName = "一年级二班" }
+            };
+
+            FieldMapBuilder builder = new FieldMapBuilder();
+            builder.Add(new FieldRowIndexMap());
+            builder.Add(new FieldMap<Student>(a => a.Name, "姓名"));
+            builder.Add(new FieldMap<Student>(a => a.ClassName, "班级"));
+            builder.Add(new FieldMap("Name", "姓名"));
+            builder.Add(new FieldMap("ClassName", "班级"));
+
+            List<FieldMap> fieldMaps = builder.FieldMaps;
+            ExcelHelper.ExportToFile(students, fieldMaps, @"D:\1.xlsx");
+        }
+    }
+}
---
+++ Jc.Util.Excel.LocalTests/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jc.Util.Excel.LocalTests
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string ClassName { get; set; }        
+    }
+}
